386b5a504fefdf865e3a5216180b11a96d69a24f
carlosmonroyg <basesdedatosucc1920@gmail.com>
2022-11-13 10:15:40 -0500

    filter

---
+++ src/api.js
@@ -0,0 +1,40 @@
+const url = 'https://api.coincap.io/v2'
+
+function getAssets() {
+  return getRequest('assets?limit=20')
+}
+
+function getRequest(method) {
+  return fetch(`${url}/${method}`, {
+    mode: 'cors',
+    method: 'GET',
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(response => {
+      return response.data
+    })
+    .catch(error => {
+      console.log('request failed', error)
+    }) // Syntax error: unexpected end of input
+}
+function getAsset(coin) {
+  return fetch(`${url}/assets${coin}`, {
+    mode: 'cors',
+    method: 'GET',
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(response => {
+      return response.data
+    })
+    .catch(error => {
+      console.log('request failed', error)
+    }) // Syntax error: unexpected end of input
+}
+
+export default { getAssets, getAsset }
